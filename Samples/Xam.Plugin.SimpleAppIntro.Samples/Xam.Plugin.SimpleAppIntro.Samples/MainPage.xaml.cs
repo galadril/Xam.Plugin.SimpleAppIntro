13d1d20682c50d9e307055e7a4acf17210f7f309
galadril <heinis.mark@gmail.com>
2018-06-26 13:33:16 +0200

    Reworked the sample main page

--- Samples/Xam.Plugin.SimpleAppIntro.Samples/Xam.Plugin.SimpleAppIntro.Samples/MainPage.xaml.cs
+++ Samples/Xam.Plugin.SimpleAppIntro.Samples/Xam.Plugin.SimpleAppIntro.Samples/MainPage.xaml.cs
@@ -9,12 +9,68 @@ using Xamarin.Forms.Xaml;
 
 namespace Xam.Plugin.SimpleAppIntro.Samples
 {
+   /// <summary>
+   /// Main Page
+   /// </summary>
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MainPage : ContentPage
 	{
+      /// <summary>
+      /// Simple main page
+      /// </summary>
 		public MainPage ()
 		{
 			InitializeComponent ();
 		}
-	}
+
+      #region Private
+
+      /// <summary>
+      /// Open the welcome page
+      /// </summary>
+      private void Open_Clicked(object sender, EventArgs e)
+      {
+         var welcomePage = new SimpleAppIntro();
+
+         // Add some simple slides
+         welcomePage.AddSlide("Welcome", "This is a sample app showing off the new App Intro", "cup_icon.png");
+         welcomePage.AddSlide("Slides", "You can add slides and have a clean app intro", "cup_icon.png");
+         welcomePage.AddSlide("Other", "Tell your user what they can do with your app", "cup_icon.png");
+
+         // Properties
+         welcomePage.ShowPositionIndicator = true;
+         welcomePage.ShowSkipButton = true;
+         welcomePage.DoneText = "Finish";
+         welcomePage.SkipText = "Skip";
+
+         // Theming
+         welcomePage.BarColor = "#607D8B";
+         welcomePage.SkipButtonBackgroundColor = "#FF9700";
+         welcomePage.DoneButtonBackgroundColor = "#8AC149";
+
+         // Callbacks
+         welcomePage.OnSkipButtonClicked = OnSkipButtonClicked;
+         welcomePage.OnDoneButtonClicked = OnDoneButtonClicked;
+
+         Navigation.PushModalAsync(welcomePage);
+      }
+
+      /// <summary>
+      /// On skip button clicked
+      /// </summary>
+      private void OnSkipButtonClicked()
+      {
+         Console.Write("Skip button clicked");
+      }
+
+      /// <summary>
+      /// On done button clicked
+      /// </summary>
+      private void OnDoneButtonClicked()
+      {
+         Console.Write("Done button clicked");
+      }
+
+      #endregion
+   }
 }
