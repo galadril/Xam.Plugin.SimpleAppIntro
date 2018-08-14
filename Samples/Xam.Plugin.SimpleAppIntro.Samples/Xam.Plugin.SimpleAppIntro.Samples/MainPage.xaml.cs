5a128a3cee5673819098fc0de42f9d68c9d4e185
galadril <heinis.mark@gmail.com>
2018-08-14 10:52:10 +0200

    Add example for static and animated simpleappintro

--- Samples/Xam.Plugin.SimpleAppIntro.Samples/Xam.Plugin.SimpleAppIntro.Samples/MainPage.xaml.cs
+++ Samples/Xam.Plugin.SimpleAppIntro.Samples/Xam.Plugin.SimpleAppIntro.Samples/MainPage.xaml.cs
@@ -25,6 +25,49 @@ namespace Xam.Plugin.SimpleAppIntro.Samples
 
       #region Private
 
+
+      /// <summary>
+      /// Open the welcome page
+      /// </summary>
+      private void Open_Static_Clicked(object sender, EventArgs e)
+      {
+         var welcomePage = new SimpleAppIntro();
+
+         // Add some simple slides
+         welcomePage.AddSlide("Welcome", "This is a sample app showing off the new App Intro", "cup_icon.png",
+            null, "#FFFFFF", "#FFFFFF",
+            FontAttributes.Bold, FontAttributes.Italic);
+         welcomePage.AddSlide("Slides", "You can add slides and have a clean app intro", "cup_icon.png",
+            null, "#FFFFFF", "#FFFFFF",
+            FontAttributes.Bold, FontAttributes.Italic);
+         welcomePage.AddSlide("Other", "Tell your user what they can do with your app", "cup_icon.png",
+            null, "#FFFFFF", "#FFFFFF",
+            FontAttributes.Bold, FontAttributes.Italic);
+
+         // Properties
+         welcomePage.ShowPositionIndicator = true;
+         welcomePage.ShowSkipButton = true;
+         welcomePage.ShowNextButton = true;
+         welcomePage.DoneText = "Finish";
+         welcomePage.NextText = "Next";
+         welcomePage.SkipText = "Skip";
+
+         // Theming
+         welcomePage.BarColor = "#607D8B";
+         welcomePage.SkipButtonBackgroundColor = "#FF9700";
+         welcomePage.DoneButtonBackgroundColor = "#8AC149";
+         welcomePage.NextButtonBackgroundColor = "#8AC149";
+
+         // Use images instead of buttons
+         welcomePage.DoneButtonImage = "baseline_done_white_24.png";
+
+         // Callbacks
+         welcomePage.OnSkipButtonClicked = OnSkipButtonClicked;
+         welcomePage.OnDoneButtonClicked = OnDoneButtonClicked;
+
+         Navigation.PushModalAsync(welcomePage);
+      }
+
       /// <summary>
       /// Open the welcome page
       /// </summary>
