da81d84ab57cbe1b87d5f8e45aad6f9e590b1536
Mark Heinis <m.heinis@jfhillebrand.com>
2018-08-15 09:51:24 +0200

    Move some code

--- Xam.Plugin.SimpleAppIntro/Views/AnimatedSlideView.xaml.cs
+++ Xam.Plugin.SimpleAppIntro/Views/AnimatedSlideView.xaml.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Xam.Plugin.SimpleAppIntro.Controls
+namespace Xam.Plugin.SimpleAppIntro.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class SlideView : ContentView
+	public partial class AnimatedSlideView : ContentView
 	{
-		public SlideView ()
+		public AnimatedSlideView ()
 		{
 			InitializeComponent ();
 
